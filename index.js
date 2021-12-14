7adec1e11cb55fdc1a11c7371b44eb6ff0d58269
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-12-14 16:35:28 +0530

    Revert the files.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/** Hello  */
-import Button from './src/button';
-import Grid from './src/grid';
-import Search from './src/search';
-import CategoryList from './src/category-list';
-import ToggleDropdown from './src/toggle-dropdown';
-import Logo from './src/logo';
-import Tooltip from './src/tooltip';
-import SuggestionList from './src/suggession-list';
-import Toaster from './src/toaster';
-
 export {
 	Button,
 	Grid,
@@ -19,4 +8,4 @@ export {
 	Tooltip,
 	SuggestionList,
 	Toaster
-}
+}  from './src/index.js';
