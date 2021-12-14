511f2622a81b0f6e6c4c793906ff83a8c7e2a728
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-12-14 16:32:56 +0530

    Added a grid skeleton

--- src/grid/index.js
+++ src/grid/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __ } from '@wordpress/i18n';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 // Internal Dependencies.
 import { ICONS } from '../icons';
@@ -10,6 +11,7 @@ import Tooltip from '../tooltip';
 
 // Grid Component
 const Grid = ( {
+	skeleton,
 	className,
 	column,
 	options,
@@ -18,9 +20,8 @@ const Grid = ( {
 	onFavoriteClick,
 	favoriteList,
 } ) => {
-	if ( ! options.length ) {
-		return '';
-	}
+
+	let hasSkeleton = skeleton ? skeleton : false;
 
 	return (
 		<div
@@ -31,75 +32,118 @@ const Grid = ( {
 			` }
 			column={ column }
 		>
-			{ options.map( ( item, index ) => {
-				const isFavorite =
-					favoriteList && favoriteList.length
-						? favoriteList.includes( `id-${ item.id }` )
-						: false;
+			{
+				hasSkeleton ? (
+					<>
+						{
+							[1, 2, 3, 4].map( (index) => {
+								return (
+									<div className="stc-grid-item" key={ index }>
+										<div className="stc-grid-item-inner">
+											<div className="stc-grid-item-screenshot" />
+											<div className="stc-grid-item-header">
+												<div className="stc-grid-item-title">
+													<Skeleton
+														variant="rect"
+														width={ 100 }
+														height={ 20 }
+														animation="wave"
+													/>
+												</div>
 
-				return (
-					<div className="stc-grid-item" key={ index }>
-						<div className="stc-grid-item-inner">
-							{ item.badge ? (
-								<div className="stc-grid-item-badge">
-									{ item.badge }
-								</div>
-							) : null }
-
-							<div
-								className="stc-grid-item-screenshot"
-								style={ {
-									backgroundImage: `url(${ item.image })`,
-								} }
-								onClick={ ( event ) => {
-									if ( 'function' === typeof onClick ) {
-										onClick( event, item );
-									}
-								} }
-							/>
-
-							<div className="stc-grid-item-header">
-								<div className="stc-grid-item-title">
-									{ decodeEntities( item.title ) }
-								</div>
-
-								{ hasFavorite ? (
-									<Tooltip
-										content={ `${
-											! isFavorite
-												? __(
-														'Add to favorites',
-														'astra-sites'
-												  )
-												: ''
-										}` }
-									>
-										<div
-											className={ `stc-grid-favorite ${
-												isFavorite ? 'active' : ''
-											}` }
-											onClick={ ( event ) => {
-												if (
-													'function' ===
-													typeof onFavoriteClick
-												) {
-													onFavoriteClick(
-														event,
-														item,
-														! isFavorite
-													);
+												{
+													hasFavorite && (
+														<div className='stc-grid-favorite'>
+															{ ICONS.favorite }
+														</div>
+													)
 												}
-											} }
-										>
-											{ ICONS.favorite }
+											</div>
 										</div>
-									</Tooltip>
-								) : null }
-							</div>
-						</div>
-					</div>
-				);
-			} ) }
+									</div>
+								);
+							})
+						}
+					</>
+				) : (
+					<>
+					{
+						options.length ? (
+							<>
+								{ options.map( ( item, index ) => {
+									const isFavorite = favoriteList && favoriteList.length ? favoriteList.includes( `id-${ item.id }` ) : false;
+
+									return (
+										<div className="stc-grid-item" key={ index }>
+											<div className="stc-grid-item-inner">
+												{ item.badge ? (
+													<div className="stc-grid-item-badge">
+														{ item.badge }
+													</div>
+												) : null }
+
+												<div
+													className="stc-grid-item-screenshot"
+													style={ {
+														backgroundImage: `url(${ item.image })`,
+													} }
+													onClick={ ( event ) => {
+														if ( 'function' === typeof onClick ) {
+															onClick( event, item );
+														}
+													} }
+												/>
+
+												<div className="stc-grid-item-header">
+													<div className="stc-grid-item-title">
+														{ decodeEntities( item.title ) }
+													</div>
+
+													{ hasFavorite ? (
+														<Tooltip
+															content={ `${
+																! isFavorite
+																	? __(
+																			'Add to favorites',
+																			'astra-sites'
+																	)
+																	: ''
+															}` }
+														>
+															<div
+																className={ `stc-grid-favorite ${
+																	isFavorite ? 'active' : ''
+																}` }
+																onClick={ ( event ) => {
+																	if (
+																		'function' ===
+																		typeof onFavoriteClick
+																	) {
+																		onFavoriteClick(
+																			event,
+																			item,
+																			! isFavorite
+																		);
+																	}
+																} }
+															>
+																{ ICONS.favorite }
+															</div>
+														</Tooltip>
+													) : null }
+												</div>
+											</div>
+										</div>
+									);
+								} ) }
+							</>
+						) : (
+							null
+						) }
+					</>
+				)
+			}
+
 		</div>
 	);
 };
