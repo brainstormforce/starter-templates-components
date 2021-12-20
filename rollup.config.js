c226318d0cc717d35dd158dc89fec13fb8f0d2ce
Nikhil <2931091+Nikschavan@users.noreply.github.com>
2021-12-20 18:55:01 +0530

    Build process using rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from "rollup-plugin-postcss";
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+        postcss({
+        plugins: [],
+        minimize: true,
+        }),
+        babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react']
+        }),
+        external(),
+        resolve(),
+        terser(),
+    ],
+  }
+];
