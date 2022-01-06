de49fb18073e98484ca0e000c36694b514453fff
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2022-01-06 14:13:31 +0600

    Upgraded Tooltip component

--- src/tooltip/index.js
+++ src/tooltip/index.js
@@ -1,16 +1,47 @@
 // External Dependencies.
-import React from 'react';
+import React, { useState } from 'react';
 
 // Internal Dependencies.
 import './style.scss';
 
 const Tooltip = ( { children, content } ) => {
+	const [ isVisible, setIsVisible ] = useState( false );
+	const [ timeOutRef, setTimeOutRef ] = useState( null );
+
+	const visibleTooltip = () => {
+		setIsVisible( true );
+	};
+
+	const autoHideTooltip = () => {
+		setTimeOutRef(
+			setTimeout( () => {
+				setIsVisible( false );
+			}, 50 )
+		);
+	};
+
+	const clearAutoHide = () => {
+		clearTimeout( timeOutRef );
+	};
+
 	return (
-		<div className="stc-tooltip">
+		<div
+			className="stc-tooltip"
+			onMouseEnter={ visibleTooltip }
+			onMouseLeave={ autoHideTooltip }
+		>
 			{ children }
 
 			{ content ? (
-				<div className="stc-tooltip-content">{ content }</div>
+				<div
+					className={ `stc-tooltip-content ${
+						isVisible ? 'stc-tooltip-visible' : 'stc-tooltip-hidden'
+					}` }
+					onMouseEnter={ clearAutoHide }
+					onMouseLeave={ autoHideTooltip }
+				>
+					{ content }
+				</div>
 			) : null }
 		</div>
 	);
