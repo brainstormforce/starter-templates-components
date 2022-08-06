8b2dcc27039a42da5dd1c978bc0c0e6a7094d31e
raojip <raojip@users.noreply.github.com>
2022-08-06 12:58:46 +0000

    Commit ESLint auto fixes

--- src/mega-menu/index.js
+++ src/mega-menu/index.js
@@ -24,7 +24,12 @@ const MegaMenu = ( { options, parent, menu, onClick } ) => {
 					setToggle( ! toggle );
 				} }
 			>
-				<span>Categories <span className='st-categories-icon'>{ ICONS.dropdown }</span></span>
+				<span>
+					Categories{ ' ' }
+					<span className="st-categories-icon">
+						{ ICONS.dropdown }
+					</span>
+				</span>
 			</div>
 
 			<div className="stc-mega-menu-container">
